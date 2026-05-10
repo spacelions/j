009ca28b29f6faff93b8202e3660ea65dec94414
Fred Zhang <fred.zhang@workstream.is>
2026-05-10 11:08:03 +0800

    chore(coverage)[SPA-82]: remove stale cli allowlist entries

--- testcases/coverage_allowlist_does_not_grow_test.go
+++ testcases/coverage_allowlist_does_not_grow_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-const allowlistCeiling = 177
+const allowlistCeiling = 146
 
 func TestCoverageAllowlist_DoesNotGrow(t *testing.T) {
 	body := readRepoFile(t, "coverage.allowlist")
